f19b2d3885f59d1e3677b6847257f98f7b20ff1f
polestarhcOne <polestarhcOne@polestarhc.com>
2024-10-15 16:47:58 +0900

    html 기초 연습2

--- product.management/src/main/java/ThisIsSpringStudy/product/management/HelloController.java
+++ product.management/src/main/java/ThisIsSpringStudy/product/management/HelloController.java
@@ -1,7 +1,6 @@
 package ThisIsSpringStudy.product.management;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
@@ -9,5 +8,12 @@ public class HelloController {
     @RequestMapping("/")
     public String hello() {
         return "Welcome to <strong>DongWon Spring study World</strong>";
+    }
+
+    // pracHtml2.html과 연동
+    @RequestMapping("/article")
+    public String createArticle(@RequestParam("title") String title,
+                                @RequestParam("content") String content) {
+        return String.format("title=%s / content=%s", title, content);
     }
 }
